4da7c81e31f473d3ae4705a7a1cba92d7f801324
Edward Loveall <edward@edwardloveall.com>
2018-09-30 15:46:37 -0400

    Add habitat to configure the app

--- src/crystal-docset-generator.cr
+++ src/crystal-docset-generator.cr
@@ -1,4 +1,12 @@
 require "http"
 require "json"
+require "habitat"
 require "./crystal-docset-generator/types/*"
 require "./crystal-docset-generator/*"
+
+Cdg.configure do |settings|
+  settings.online_path = "https://crystal-lang.org/api/"
+  settings.docset_path = "crystal.docset"
+  settings.resource_path = "#{settings.docset_path}/Contents/Resources"
+  settings.docs_path = "#{settings.resource_path}/Documents"
+end
---
+++ src/crystal-docset-generator/config.cr
@@ -0,0 +1,8 @@
+module Cdg
+  Habitat.create do
+    setting online_path : String
+    setting docset_path : String
+    setting resource_path : String
+    setting docs_path : String
+  end
+end
