0cb92a5b6ea03ad00f2e978fb2eb99e6cc83e567
Edward Loveall <edward@edwardloveall.com>
2018-09-30 15:38:40 -0400

    Throw error if no version is passed

--- main.cr
+++ main.cr
@@ -1,3 +1,9 @@
 require "./crystal-docset-generator"
 
+if ARGV.empty?
+  puts "Please supply a version:"
+  puts "./crystal-docset-generator 0.1.2"
+  exit(1)
+end
+
 Cdg::Generator.new(version: ARGV[0]).generate!
