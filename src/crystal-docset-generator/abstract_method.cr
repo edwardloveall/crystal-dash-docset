19fb78eb5fe444aaef5082a8bf18f4cc0397b6e8
Edward Loveall <edward@edwardloveall.com>
2018-10-04 21:52:14 -0400

    Factor out page to be on the AbstractMethod

--- src/crystal-docset-generator/abstract_method.cr
+++ src/crystal-docset-generator/abstract_method.cr
@@ -17,4 +17,19 @@ abstract class Cdg::AbstractMethod
     SQL
     Cdg.settings.db.exec(sql, name, type, path)
   end
+
+  def page : Page
+    if page = nillable_page
+      page
+    else
+      raise <<-TEXT
+
+        The #{self.class.name} for #{name} doesn't have a page associated with
+        it. The thing is, it needs this page to get its full path. You can
+        check the src/crystal-docset-generator/page.cr file, and start with the
+        process_methods method to see what's up.
+
+      TEXT
+    end
+  end
 end
--- src/crystal-docset-generator/class_method.cr
+++ src/crystal-docset-generator/class_method.cr
@@ -3,19 +3,8 @@ class Cdg::ClassMethod < Cdg::AbstractMethod
     "Method"
   end
 
-  def path : String
-    if page = nillable_page
-      escaped_id = URI.escape(id)
-      "#{page.path}##{escaped_id}"
-    else
-      raise <<-TEXT
-
-        The #{self.class.name} for #{name} doesn't have a page associated with
-        it. The thing is, it needs this page to get the path of the class it's
-        attached to. You can check the src/crystal-docset-generator/page.cr
-        file, and start with the process_methods method to see what's up.
-
-      TEXT
-    end
+  def path
+    escaped_id = URI.escape(id)
+    "#{page.path}##{escaped_id}"
   end
 end
